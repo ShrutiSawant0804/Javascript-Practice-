dc84d35515de0726153074d646c540120c9417cc
Shruti <sawantshruti0804@gmail.com>
2025-08-13 23:44:10 +0530

    oops

--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const car = new Car('Toyota', 'Corolla', 2020); // Create an instance of Car
-// car.printDetails();  // Print details of the car
-// const car2 = new Car('Honda', 'Civic', 2021);
-// car2.printDetails();
