4a963d0787a3539de6892686447ff69ea06d91ad
Shruti <sawantshruti0804@gmail.com>
2025-08-22 12:40:28 +0530

    update

--- Opps2.js
+++ Opps2.js
@@ -4,21 +4,7 @@ class Product {
         this.price = price;
     }
 
-displayDetails() {
-    console.log("Product Name: " + this.name);
-    console.log("Product Price: $" + this.price);
-    console.log("Product Category: " + this.category);
+    displayInfo() {
+        console.log(`Product Name: ${this.name}, Price: $${this.price}`);
+    }
 }
-}
-let iphone = new Product('iPhone 14', 999); // Create an instance of Product
-
-iphone.name = 'iPhone 14 Pro'; // Update the name of the product
-iphone.price = 1099; // Update the price of the product
-iphone.category = 'Smartphone'; // Add a new property to the product
-
-console.log("Updated Product Details:");
-iphone.displayDetails(); // Display updated product details
-
-
-
-
